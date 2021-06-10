59a284a333d5a948f39c1cd7998f9cbe8f621cf7
ThiccusBootieus <bultman82@gmail.com>
2021-06-09 19:13:30 -0500

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,1 +1,15 @@
-print("big dick energy")
+from flask import Flask
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def index():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    # Debug/Development
+    # app.run(debug=True, host="0.0.0.0", port="5000")
+    # Production
+    http_server = WSGIServer(('', 5000), app)
+    http_server.serve_forever()
