c8671da5a31362dddc5ed7a5f20f66eadc172b6e
ThiccusBootieus <bultman82@gmail.com>
2021-06-09 22:13:43 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,22 +5,34 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route('/api', methods=['GET'])
+@app.route('/api/', methods=['GET'])
 def index():
     return "Im in the mainframe"
 
+@app.route('/hello2/', methods=['GET'])
+def hello2(name=None):
+    return render_template('hello2.html', name=name)
 
-@click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-@app.route('/hello/')
-@app.route('/hello/<name>')
+
+
+
+
 @app.route('/index/')
 @app.route('/index/*')
 
+@app.route('/generic/')
+@app.route('/elements/')
+
+
+
+
+
+
+#needs to be last file routed
+@app.route('/hello/')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
 
 
 
@@ -30,7 +42,6 @@ if __name__ == '__main__':
     # app.run(debug=True, host="0.0.0.0", port="5000")pip
     # Production
     http_server = WSGIServer(('', 5000), app)
-    hello()
 
     http_server.serve_forever()
 
